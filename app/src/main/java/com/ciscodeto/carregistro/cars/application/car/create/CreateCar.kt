23a91bf83c0653b9447c5bf17632fcd744a8418c
ciscodeto <netofcisco@gmail.com>
2025-07-02 20:54:40 -0300

    feat: CreateCar useCase added and implemented

--- app/src/main/java/com/ciscodeto/carregistro/cars/application/car/create/CreateCar.kt
+++ app/src/main/java/com/ciscodeto/carregistro/cars/application/car/create/CreateCar.kt
@@ -1,4 +1,7 @@
 package com.ciscodeto.carregistro.cars.application.car.create
 
+import com.ciscodeto.carregistro.cars.application.car.repository.CarDto
+
 interface CreateCar {
+    suspend fun createCar(carDto: CarDto)
 }
--- app/src/main/java/com/ciscodeto/carregistro/cars/application/car/create/CreateCarImpl.kt
+++ app/src/main/java/com/ciscodeto/carregistro/cars/application/car/create/CreateCarImpl.kt
@@ -1,4 +1,12 @@
 package com.ciscodeto.carregistro.cars.application.car.create
 
-class CreateCarImpl {
+import com.ciscodeto.carregistro.cars.application.car.repository.CarDto
+import com.ciscodeto.carregistro.cars.application.car.repository.CarRepository
+
+class CreateCarImpl(
+    private val carRepository: CarRepository
+) : CreateCar {
+    override suspend fun createCar(carDto: CarDto) {
+        carRepository.insert(carDto)
+    }
 }
